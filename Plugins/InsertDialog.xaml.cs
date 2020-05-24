83ef48608c9b4d353fd93cd817111c28ec5d8201
Colin Mothersead <cmothersead@icacontrol.com>
2020-05-24 11:07:03 -0400

    Add project files.

---
+++ Plugins/InsertDialog.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanelInsert
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class InsertDialog : Window
+    {
+        public InsertDialog()
+        {
+            InitializeComponent();
+            ComponentCollection componentCollection = (ComponentCollection)this.Resources["components"];
+            FillCollection(componentCollection);
+        }
+
+        private void FillCollection(ComponentCollection componentCollection)
+        {
+            OleDbConnection con = Connect.ProjectDatabase();
+            con.Open();
+
+            string statement = "SELECT * FROM `COMP` WHERE `PAR1_CHLD2` = '1'";
+            OleDbCommand cmd = new OleDbCommand(statement, con);
+            OleDbDataReader odr = cmd.ExecuteReader();
+
+            while (odr.Read())
+            {
+                PanelInsert.Component component = new PanelInsert.Component();
+                component.deviceNumber = odr["TAGNAME"].ToString();
+                component.manufacturer = odr["MFG"].ToString();
+                component.catalogNumber = odr["CAT"].ToString();
+                component.installation = odr["INST"].ToString();
+                component.location = odr["LOC"].ToString();
+                componentCollection.Add(component);
+            }
+        }
+
+    }
+}
---
+++ Plugins/main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.OleDb;
+using System.Windows;
+using System.Windows.Forms;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+using Autodesk.Electrical.Project;
+
+namespace PanelInsert
+{
+    public class Connect
+    {
+        public static OleDbConnection ProjectDatabase()
+        {
+            var connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + ProjectManager.GetInstance().GetActiveProject().GetDbFullPath() + "'";
+            return new OleDbConnection(connectionString);
+        }
+
+        [CommandMethod("TESTDIALOG")]
+        public static void DialogTest()
+        {
+            var dialog = new InsertDialog();
+            var result = Autodesk.AutoCAD.ApplicationServices.Application.ShowModalWindow(dialog);
+        }
+    }
+
+    public class Component
+    {
+        public string deviceNumber;
+        public string manufacturer;
+        public string catalogNumber;
+        public string installation;
+        public string location;
+    }
+
+    public class ComponentCollection : ObservableCollection<Component>
+    {
+
+    }
+}
