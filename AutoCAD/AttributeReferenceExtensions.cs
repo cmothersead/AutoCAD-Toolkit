5eae8c0e6c42377f157a333153c80802e1d1b6dc
Colin Mothersead <cmothersead@icacontrol.com>
2021-03-03 12:49:13 -0500

    Refactor base AutoCAD extensions into independent project

---
+++ AutoCAD/AttributeReferenceExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace ICA.AutoCAD
+{
+    public static class AttributeReferenceExtensions
+    {
+        public static bool Hide(this AttributeReference attributeReference)
+        {
+            try
+            {
+                using (Transaction transaction = attributeReference.Database.TransactionManager.StartTransaction())
+                {
+
+                    AttributeReference attributeReferenceForWrite = transaction.GetObject(attributeReference.ObjectId, OpenMode.ForWrite) as AttributeReference;
+                    attributeReferenceForWrite.Invisible = true;
+                    transaction.Commit();
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool Unhide(this AttributeReference attributeReference)
+        {
+            try
+            {
+                using (Transaction transaction = attributeReference.Database.TransactionManager.StartTransaction())
+                {
+
+                    AttributeReference atttributeReferenceForWrite = transaction.GetObject(attributeReference.ObjectId, OpenMode.ForWrite) as AttributeReference;
+                    atttributeReferenceForWrite.Invisible = false;
+                    transaction.Commit();
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool SetValue(this AttributeReference attributeReference, string value)
+        {
+            try
+            {
+                using (Transaction transaction = attributeReference.Database.TransactionManager.StartTransaction())
+                {
+                    AttributeReference attributeReferenceForWrite = transaction.GetObject(attributeReference.ObjectId, OpenMode.ForWrite) as AttributeReference;
+                    attributeReferenceForWrite.TextString = value;
+                    transaction.Commit();
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool SetPosition(this AttributeReference attributeReference, Point3d position)
+        {
+            try
+            {
+                using (Transaction transaction = attributeReference.Database.TransactionManager.StartTransaction())
+                {
+
+                    AttributeReference attributeReferenceForWrite = transaction.GetObject(attributeReference.ObjectId, OpenMode.ForWrite) as AttributeReference;
+                    attributeReferenceForWrite.AlignmentPoint = position;
+                    transaction.Commit();
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AutoCAD/BlockReferenceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ICA.AutoCAD
+{
+    public static class BlockReferenceExtensions
+    {
+        public static AttributeReference GetAttributeReference(this BlockReference blockReference, string tag)
+        {
+            using (Transaction transaction = blockReference.Database.TransactionManager.StartTransaction())
+            {
+                foreach (ObjectId attRefID in blockReference.AttributeCollection)
+                {
+                    AttributeReference attRef = transaction.GetObject(attRefID, OpenMode.ForRead) as AttributeReference;
+                    if (attRef.Tag == tag)
+                    {
+                        return attRef;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
