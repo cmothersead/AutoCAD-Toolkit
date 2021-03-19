44bb3ceea1c2389132d86abbd7f38ebdf24373b8
Colin Mothersead <cmothersead@icacontrol.com>
2021-03-19 09:32:53 -0400

    Create models for description that handle automatic addition/deletion of lines

---
+++ AutoCAD.Adapter.Windows/Models/DescriptionCollection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ICA.AutoCAD.Adapter.Windows.Models
+{
+    /// <summary>
+    /// Represents an <see cref="ObservableCollection{DescriptionLine}"/>  with functionality to handle insertions and deletions automatically
+    /// </summary>
+    public class DescriptionCollection : ObservableCollection<DescriptionLine>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DescriptionCollection()
+        {
+            AddEmpty();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds <paramref name="item"/> to the collection
+        /// </summary>
+        /// <param name="item"></param>
+        public new void Add(DescriptionLine item)
+        {
+            item.Emptied += DescriptionLine_Emptied;
+            Insert(Count - 1, item);
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="DescriptionLine"/> with <see cref="DescriptionLine.Value"/> of <paramref name="value"/> to the collection
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add(string value)
+        {
+            Add(new DescriptionLine(value));
+        }
+
+        /// <summary>
+        /// Unsubscribes <paramref name="item"/> from the <see cref="DescriptionLine_Emptied(object, EventArgs)"/> handler and removes it from the collection
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns><see langword="true"/> if <paramref name="item"/> is successfully removed, otherwise <see langword="false"/></returns>
+        public new bool Remove(DescriptionLine item)
+        {
+            item.Emptied -= DescriptionLine_Emptied;
+            return base.Remove(item);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Adds an empty <see cref="DescriptionLine"/> to the end of the collection
+        /// </summary>
+        private void AddEmpty()
+        {
+            DescriptionLine empty = new DescriptionLine();
+            empty.Filled += DescriptionLine_Filled;
+            Insert(Count, empty);
+        }
+
+        #endregion
+
+        #region Private Event Handlers
+
+        /// <summary>
+        /// Removes <see cref="DescriptionLine"/> from the collection upon value being set to null or empty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DescriptionLine_Emptied(object sender, EventArgs e)
+        {
+            Remove(sender as DescriptionLine);
+        }
+
+        /// <summary>
+        /// Adds a new empty <see cref="DescriptionLine"/> to the end of the collection when the current one takes on a non-empty value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DescriptionLine_Filled(object sender, EventArgs e)
+        {
+            DescriptionLine d = sender as DescriptionLine;
+            d.Filled -= DescriptionLine_Filled;
+            d.Emptied += DescriptionLine_Emptied;
+            AddEmpty();
+        }
+
+        #endregion
+    }
+}
---
+++ AutoCAD.Adapter.Windows/Models/DescriptionLine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ICA.AutoCAD.Adapter.Windows.Models
+{
+    public class DescriptionLine
+    {
+        private string _value;
+        /// <summary>
+        /// Text value of this description line. Forces all characters to uppercase
+        /// </summary>
+        public string Value
+        {
+            get => _value;
+            set
+            {
+               if(string.IsNullOrEmpty(value))
+                {
+                    Emptied?.Invoke(this, new EventArgs());
+                    return;
+                }
+                else if (string.IsNullOrEmpty(_value))
+                {
+                    Filled?.Invoke(this, new EventArgs());
+                }
+                _value = value.ToUpper();
+            }
+        }
+
+        public DescriptionLine() { }
+        public DescriptionLine(string value) => Value = value;
+
+        /// <summary>
+        /// Occurs when <see cref="Value"/> is assigned to be a null or empty string
+        /// </summary>
+        public event EventHandler Emptied;
+
+        /// <summary>
+        /// Occurs when <see cref="Value"/> takes on a non-null, non-empty value
+        /// </summary>
+        public event EventHandler Filled;
+    }
+}
